2b243b82593ee1be997bd582361cceb85c21b406
Raj Sharma <rajmwr5785@gmail.com>
2021-04-03 14:52:46 +0000

    Adapt for stag
    Signed-off-by: Raj Sharma <rajmwr5785@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/stag_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rolex-user \
-    lineage_rolex-userdebug \
-    lineage_rolex-eng
+    stag_rolex-user \
+    stag_rolex-userdebug \
+    stag_rolex-eng
